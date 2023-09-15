8dde8506987fc4e190d192f1dff6d4a10dd689cd
Kaju1204 <134523207+Kaju1204@users.noreply.github.com>
2023-09-15 18:18:38 +0530

    Add files via upload

---
+++ Assignment3.js
@@ -0,0 +1,45 @@
+    //Even&Odd number program//
+let Number=prompt("Enter your number to check");
+if(Number%2===0){
+    alert("The number is even")
+}else{
+    alert("The number is odd.")
+}
+
+    //Campare two number//
+let First_Number=prompt("Enter Fisrt Number");
+let Second_Number=prompt("Enter Second Number");
+if(First_Number>Second_Number){
+    console.log("First number is greater than second number");
+}else{
+    console.log("Second number is graater than first number")
+}
+
+    //Campare with 10 and check//
+let Number=prompt("Enter your number");
+if(Number<10){
+    console.log("The Number is less than 10.")
+}else{
+    console.log("The number is greater than equal to 10.")
+}
+
+    //1 to 10 number//
+let num=10;
+for(let i=1; i<=10; i++){
+    console.log(i)
+}
+
+    //1 to 10 even number//
+let num=10
+for(let i=1; i<=10; i++){
+    if(i%2==0)
+    console.log(i)
+}
+
+    //1 to 10 odd number//
+ let num=10
+for(let i=1; i<=10; i++){
+        if(i%2!==0)
+        console.log(i)
+}
+    
